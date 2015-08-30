03bfa010751f582e158be0666f8520d89f948ae6
王信凱 <jcskyting@gmail.com>
2015-08-31 00:26:42 +0800

    index.js add ask/answer channel

--- index.js
+++ index.js
@@ -20,6 +20,14 @@ io.on('connection', function(socket){
     io.emit('chat message', msg);
     console.log('io.emit='+msg);
   });
+  socket.on('ask_channel', function(msg){
+    io.emit('ask_channel', msg);
+    console.log('ask_channel='+msg);
+  });
+  socket.on('answer_channel', function(msg){
+    io.emit('answer_channel', msg);
+    console.log('answer_channel='+msg);
+  });
 });
 
 http.listen(3000, function(){
